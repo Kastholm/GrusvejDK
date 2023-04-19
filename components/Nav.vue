706f12b9fe81ae0c7f38406690a8f8e58859b558
Marc <marc@aguardio.com>
2023-04-19 14:42:11 +0200

    Added indexPage from template as temporary

--- components/Nav.vue
+++ components/Nav.vue
@@ -13,7 +13,7 @@
             />
           </div>
         </div>
-        <div
+        <!-- <div
           class="relative z-0 flex flex-1 items-center justify-center px-2 sm:absolute sm:inset-0"
         >
           <div class="w-full sm:max-w-xs">
@@ -36,7 +36,7 @@
               />
             </div>
           </div>
-        </div>
+        </div> -->
         <div class="relative z-10 flex items-center lg:hidden">
           <!-- Mobile menu button -->
           <DisclosureButton
@@ -186,7 +186,7 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 const navigation = [
-  { name: "Dashboard", href: "#", current: true },
+  { name: "Forside", href: "#", current: true },
   { name: "Team", href: "#", current: false },
   { name: "Projects", href: "#", current: false },
   { name: "Calendar", href: "#", current: false },
