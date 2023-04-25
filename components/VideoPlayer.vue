836cadc3474510b121e6ff376418d5145a51e411
Anton Christensen <webbermanden@gmail.com>
2023-04-25 22:58:31 +0200

    VideoPlayer størrelse indstillet

--- components/VideoPlayer.vue
+++ components/VideoPlayer.vue
@@ -1,19 +1,13 @@
 <template>
-    <div class="relative">
-    <video ref="videoPlayer" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1" @play="onPlay" @pause="onPause" class="w-full h-full"></video>
-    <div class="absolute inset-0 flex justify-center items-center">
-      <button @click="play" class="bg-white rounded-full w-12 h-12 flex justify-center items-center">
-        <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path d="M5 3l14 9-14 9V3z"></path>
-        </svg>
-      </button>
-    </div>
+    <div class="h-80 max-w-3xl">
+    <iframe ref="videoPlayer" src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1&mute=1" @play="onPlay" @pause="onPause" class="w-full h-full"></iframe>
+    
   </div>
 </template>
 
 <script>
 export default {
-
+ 
 }
 </script>
 
