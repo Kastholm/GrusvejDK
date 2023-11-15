f75fb9d5ae5b7f40b04e8b853e8c5170924d6403
Marc <marc@aguardio.com>
2023-11-15 16:35:16 +0100

    finished form

--- server/server.ts
+++ server/server.ts
@@ -11,40 +11,47 @@ app.use(express.json());
 app.use(cors());
 // Lav api end points
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
-app.post( '/send-email', async ( req, res ) => {
-     let transporter = nodemailer.createTransport({
-           service: 'outlook',
-           auth: {
-                user: 'Christiansen95@live.dk',
-                pass: ''
-           }
-      });
+app.post('/send-email', async (req, res) => {
+    let transporter = nodemailer.createTransport({
+        service: 'outlook',
+        auth: {
+            user: 'Christiansen95@live.dk',
+            pass: 'K@stholm9fem'
+        }
+    });
 
 
-let mailOptions = {
-     from: process.env.EMAIL_USER,
-     to: process.env.EMAIL_USER, // Dette vil sende e-mailen til dig
-     subject: 'Form submission',
-     text: `
-         Name: ${req.body.name},
-         Email: ${req.body.email},
-         Phone: ${req.body.phone},
-         Grusvej: ${req.body.grusvej}
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER, // Dette vil sende e-mailen til dig
+        subject: 'Form submission',
+        text: `
+         Navn: ${req.body.name},
+        
+         E-mail: ${req.body.email},
+
+         Telefon: ${req.body.phone},
+
+         Valgte Grusvej: ${req.body.grusvej},
+
+         Besked: 
+         
+         ${req.body.text}
      `
- };
+    };
 
- transporter.sendMail(mailOptions, (error, info) => {
-     if (error) {
-         console.log(error);
-         res.status(500).send('Internal Server Error');
-     } else {
-         console.log('Email sent: ' + info.response);
-         res.status(200).send('Email sent successfully!');
-     }
- });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully!');
+        }
+    });
 });
 // Start Express server
 const PORT = 4000;
