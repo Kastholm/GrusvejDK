2b36f3e1fbbe6101f5aeb49d59e219b113e26f1f
Anton B. Christensen <webbermanden@gmail.com>
2025-09-28 20:04:59 +0200

    fixed defu version

--- server/api/send.ts
+++ server/api/send.ts
@@ -1,7 +1,6 @@
 import { Resend } from "resend";
 
-//const resend = new Resend(process.env.RESEND_API_KEY);
-const resend = new Resend("dummy key");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default defineEventHandler(async (event) => {
   const { email, phone, name } = await readBody(event);
