0f5130685ea28a2bcd1391b8c212b976ecbb4018
Anton B. Christensen <webbermanden@gmail.com>
2025-09-28 20:38:47 +0200

    reinserted env var to resend

--- server/api/send.ts
+++ server/api/send.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 
 // process.env.RESEND_API_KEY
-const resend = new Resend("dfas");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default defineEventHandler(async (event) => {
   const { email, phone, name } = await readBody(event);
