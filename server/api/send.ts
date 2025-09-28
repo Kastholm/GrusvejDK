b72dac1f1d440446f9ed70fcbb2a6bf09a3b3e39
Anton B. Christensen <webbermanden@gmail.com>
2025-09-28 20:37:20 +0200

    node updated, auth adn axios deleted

--- server/api/send.ts
+++ server/api/send.ts
@@ -1,6 +1,7 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// process.env.RESEND_API_KEY
+const resend = new Resend("dfas");
 
 export default defineEventHandler(async (event) => {
   const { email, phone, name } = await readBody(event);
