334b277c7b8677c3ed83af87bdce3b2ac39f336a
Anton B. Christensen <webbermanden@gmail.com>
2026-02-09 14:08:27 +0100

    test 4 ny mappe

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,19 +1,18 @@
 <template>
   <head>
     <title>Kontakt Grusvej.dk og få en god dialog om din grusvej</title>
-    <meta name="description" content="Kontakt Grusvej.dk og hør mere - vi er eksperter i grusveje, cement og naturstier. Modtag et uforpligtende tilbud eller få godt råd med på vejen.">
+    <meta
+      name="description"
+      content="Kontakt Grusvej.dk og hør mere - vi er eksperter i grusveje, cement og naturstier. Modtag et uforpligtende tilbud eller få godt råd med på vejen."
+    />
   </head>
   <div>
-   <Kontaktside />
-    </div>
+    <Kontaktside />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
